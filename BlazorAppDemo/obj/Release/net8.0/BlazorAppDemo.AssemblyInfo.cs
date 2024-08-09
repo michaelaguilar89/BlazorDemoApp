00f955fea867ad160a702fd87c776bee3926867f
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2024-08-09 10:18:43 -0600

    update publis profile and docker image

--- BlazorAppDemo/obj/Release/net8.0/BlazorAppDemo.AssemblyInfo.cs
+++ BlazorAppDemo/obj/Release/net8.0/BlazorAppDemo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79120b570e3437c19a05ea36d46bdfbb96f70e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
